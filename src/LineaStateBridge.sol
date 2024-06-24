08db21a1405ad98d46145e5a53d13d0b36cadd39
kfastov <kfastov@users.noreply.github.com>
2024-06-24 15:30:20 +0300

    feat: initial commit

---
+++ src/LineaStateBridge.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+// Optimism interface for cross domain messaging
+
+/// @title World ID State Bridge Optimism
+/// @author Worldcoin
+/// @notice Distributes new World ID Identity Manager roots to the Linea chain
+/// @dev This contract lives on Ethereum mainnet and works for Linea
+contract LineaStateBridge is Ownable2Step {
+    // solhint-disable-next-line no-empty-blocks
+    constructor() { }
+
+    // TODO: implement contract logic
+}
