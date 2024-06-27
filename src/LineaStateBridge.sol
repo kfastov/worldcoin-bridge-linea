6271e90b4b499388d7d326867c3deef40bcf8896
Ugo-X <ugonnadike001@gmail.com>
2024-06-27 06:55:37 +0100

    gas limits set

--- src/LineaStateBridge.sol
+++ src/LineaStateBridge.sol
@@ -12,4 +12,37 @@ contract LineaStateBridge is Ownable2Step {
     constructor() { }
 
     // TODO: implement contract logic
+    ///////////////////////////////////////////////////////////////////
+///                         Linea GAS LIMIT                     ///
+///////////////////////////////////////////////////////////////////
+
+/// @notice Sets the gas limit for the propagateRoot method
+/// @param _lineaGasLimit The new gas limit for the propagateRoot method
+function setGasLimitPropagateRoot(uint32 _lineaGasLimit) external onlyOwner {
+    if (_lineaGasLimit <= 0) {
+        revert GasLimitZero();
+    }
+    gasLimitPropagateRoot = _lineaGasLimit;
+    emit SetGasLimitPropagateRoot(_lineaGasLimit);
+}
+
+/// @notice Sets the gas limit for the setRootHistoryExpiry method
+/// @param _lineaGasLimit The new gas limit for the setRootHistoryExpiry method
+function setGasLimitSetRootHistoryExpiry(uint32 _lineaGasLimit) external onlyOwner {
+    if (_lineaGasLimit <= 0) {
+        revert GasLimitZero();
+    }
+    gasLimitSetRootHistoryExpiry = _lineaGasLimit;
+    emit SetGasLimitSetRootHistoryExpiry(_lineaGasLimit);
+}
+
+/// @notice Sets the gas limit for the transferOwnership method
+/// @param _lineaGasLimit The new gas limit for the transferOwnership method
+function setGasLimitTransferOwnership(uint32 _lineaGasLimit) external onlyOwner {
+    if (_lineaGasLimit <= 0) {
+        revert GasLimitZero();
+    }
+    gasLimitTransferOwnership = _lineaGasLimit;
+    emit SetGasLimitTransferOwnership(_lineaGasLimit);
+}
 }
