f90cfd2b699d528275212981e646634769903fa9
Ugo-X <ugonnadike001@gmail.com>
2024-08-03 02:43:21 +0100

    deleted comments

--- src/interfaces/ICrossDomainOwnableLinea.sol
+++ src/interfaces/ICrossDomainOwnableLinea.sol
@@ -1,9 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-/// @title Interface for the LineaWorldID contract
-/// @author Worldcoin and 0xprinc x James Harrison.
-/// @notice Interface for the MessageServiceBase contract for the Linea L2
 interface ICrossDomainOwnableLinea {
     /// @notice Allows for ownership to be transferred with specifying the locality.
     /// @param _owner   The new owner of the contract.
