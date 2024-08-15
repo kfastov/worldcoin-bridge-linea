42a4c9360b156f1969800bb1e6d0d72eb7ae1561
Konstantin Fastov <kfastov@gmail.com>
2024-08-15 14:11:18 +0300

    docs: remove leftovers from Optimism

--- src/LineaWorldID.sol
+++ src/LineaWorldID.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.19;
 import { WorldIDBridge } from "world-id-state-bridge/abstract/WorldIDBridge.sol";
 import { ILineaWorldID } from "./interfaces/ILineaWorldID.sol";
 import { CrossDomainOwnableLinea } from "./CrossDomainOwnableLinea.sol";
-// import { CrossDomainOwnable3 } from "@eth-optimism/contracts-bedrock/contracts/L2/CrossDomainOwnable3.sol";
 
 /// @title Linea World ID Bridge
 /// @author Worldcoin and 0xprinc
@@ -33,12 +32,9 @@ contract LineaWorldID is WorldIDBridge, ILineaWorldID, CrossDomainOwnableLinea {
 
     /// @notice This function is called by the state bridge contract when it forwards a new root to
     ///         the bridged WorldID.
-    /// @dev    This function can revert if Optimism's CrossDomainMessenger stops processing proofs
-    ///         or if OPLabs stops submitting them. Next iteration of Optimism's cross-domain messaging, will be
-    ///         fully permissionless for message-passing, so this will not be an issue.
-    ///         Sequencer needs to include changes to the CrossDomainMessenger contract on L1,
-    ///         not economically penalized if messages are not included, however the fraud prover (Cannon)
-    ///          can force the sequencer to include it.
+    /// @dev    This function relies on Linea's cross-domain messaging system. It can only be called
+    ///         by the owner (which should be set to the L1 bridge contract address) to ensure
+    ///         that roots are only updated through the official bridging process.
     ///
     /// @param newRoot The value of the new root.
     ///
