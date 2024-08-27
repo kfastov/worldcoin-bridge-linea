b0e13d22540c3d081d9456482e1274a9878c098d
adrianvrj <adrivargas48@gmail.com>
2024-08-27 10:27:54 -0600

    [fix] added JSDoc documentation and l2 rpc url emptiness handling

--- src/script/js/claimMessageModule.js
+++ src/script/js/claimMessageModule.js
@@ -10,14 +10,33 @@ if (!privateKey) {
     throw new Error('Private key not found in environment variables');
 }
 
+const l2RPCUrl = process.env.L2_RPC_URL;
+if (!l2RPCUrl) {
+  throw new Error('Layer 2 RPC url not found in environment variables');
+}
+
 // Initialize the Linea SDK
 const lineaSDK = new LineaSDK({
-  l2RpcUrl: process.env.L2_RPC_URL ?? "",
-  l2SignerPrivateKey: process.env.L2_SIGNER_PRIVATE_KEY ?? "",
+  l2RpcUrl: l2RPCUrl,
+  l2SignerPrivateKey: privateKey,
   network: "linea-mainnet",
   mode: "read-write",
 });
 
+
+/**
+ * Claims a pending message on L2MessageService
+ * @param {string} sender - The address of the message sender
+ * @param {string} destination - The destination address
+ * @param {string} fee - The fee for claiming the message
+ * @param {string} value - The value of the message
+ * @param {string} messageHash - The hash of the message
+ * @param {string} nonce - The nonce of the message
+ * @param {string} calldata - The calldata of the message
+ * @param {string} l2MessageServiceAddress - The address of the L2MessageService contract
+ * @returns {Promise<ethers.ContractTransaction>} The transaction receipt
+ * @throws {Error} If there's an error during the claiming process
+ */
 async function claimPendingMessage(sender, destination, fee, value, messageHash, nonce, calldata, l2MessageServiceAddress) {
     try {
       const l2Contract = lineaSDK.getL2Contract(l2MessageServiceAddress);
